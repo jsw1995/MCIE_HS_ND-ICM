43a1d98ec4c1a03d13dbe493cf3f5b11e45ff524
jsw1995 <87681599+jsw1995@users.noreply.github.com>
2024-09-23 11:01:09 +0800

    Update MAIN.m

--- MAIN22.m
+++ MAIN22.m
@@ -1,7 +1,7 @@
-%% Ö±·½Í¼ÒÆÎ»Ç¶ÈëÊµÑé·ÂÕæ
+%% ç›´æ–¹å›¾ç§»ä½åµŒå…¥å®éªŒä»¿çœŸ
 clc;clear all;close all;clear;
 
-% ±¾ÎÄ¼Ó½âÃÜ½á¹û
+% æœ¬æ–‡åŠ è§£å¯†ç»“æœ
 im = double(imread('date/woman.tif'));
 cover = double(imread('date/bridge.bmp'));
  
@@ -20,30 +20,19 @@ cover = double(imread('date/bridge.bmp'));
 key = '8d5ab8ba5340fce4420829ad5d12a0e45dacb0858544163d04c1d02b73e3697d';
 cr=0.5;
 
-emb_type = 1;  % ¿ÕÓò
+emb_type = 1;  % ç©ºåŸŸ
 tt=11;
 
-% emb_type = 2;  %Ğ¡²¨
+% emb_type = 2;  %å°æ³¢
 % tt=11;
 
 [M,N,K] = size(cover);
 [m,n,k] = size(im);
 im_shape = [m,n,k];
 
-
 [ cip,dnkey,mmr,TT,tcip,rC ] = encryption( im,cover,key,cr,emb_type,tt );
 [ rim,rtcip ] = dencryption( cip,cover,dnkey,mmr,TT,cr,emb_type,im_shape,rC );
-
-
-[mssim_cip] = mssim(cover,cip,M-8);
-[ssim_cip, ~] = ssim(uint8(cover),uint8(cip));
-[psnr_cip, ~] = psnr(cover,double(uint8(cip)),255);
-
-[mssim_rim] = mssim(im,rim,m-8);
-[ssim_rim, ~] = ssim(uint8(im),uint8(rim));
-[psnr_rim, ~] = psnr(im,double(uint8(rim)),255);
  
-
 figure(1)
 imshow(uint8(im))
 figure(2)
